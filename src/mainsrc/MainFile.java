b75ef0e6cd56df2df2d1d9f6dceb4ebd9d57b242
valeriodng <valeriodng@gmail.com>
2017-02-12 19:29:05 +0100

    Caricamento iniziale

---
+++ src/mainsrc/MainFile.java
@@ -0,0 +1,13 @@
+package mainsrc;
+
+public class MainFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Ciao Mondo!!!");	
+	
+	
+	}
+
+}
